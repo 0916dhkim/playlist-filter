270fb69d5a42b0a8d6e4a5194aed5c3989974c55
Peanut <topeanutz@gmail.com>
2022-11-06 21:19:50 -0500

    refactor: remove useEffect and add inline variables to style slider

--- app/src/components/MinMaxSlider.tsx
+++ app/src/components/MinMaxSlider.tsx
@@ -1,47 +1,22 @@
-import { ReactElement,useCallback, useEffect, useRef } from "react";
+import { ReactElement,useCallback} from "react";
 import { useAtom, PrimitiveAtom } from "jotai";
 import "./MinMaxSlider.css";
 
 type MinMaxSliderProps = {
   sliderMin: number;
   sliderMax: number;
-  minAtom: PrimitiveAtom<number>;
-  maxAtom: PrimitiveAtom<number>;
+  minInputAtom: PrimitiveAtom<number>;
+  maxInputAtom: PrimitiveAtom<number>;
 };
-export default function MinMaxSlider({sliderMin,sliderMax,minAtom,maxAtom}:MinMaxSliderProps):ReactElement {
-    const [minVal, setMinVal] = useAtom(minAtom);
-    const [maxVal, setMaxVal] = useAtom(maxAtom);
-    const minValRef = useRef(sliderMin);
-    const maxValRef = useRef(sliderMax);
-    const range = useRef<HTMLDivElement>(null);
+export default function MinMaxSlider({sliderMin,sliderMax,minInputAtom,maxInputAtom}:MinMaxSliderProps):ReactElement {
+    const [minVal, setMinVal] = useAtom(minInputAtom);
+    const [maxVal, setMaxVal] = useAtom(maxInputAtom);
     
     // Convert to percentage
     const getPercent = useCallback(
       (value:number) => Math.round(((value - sliderMin) / (sliderMax - sliderMin)) * 100),
       [ sliderMin,  sliderMax]
     );
-  
-    // Set width of the range to decrease from the left side **
-    useEffect(() => {
-      const minPercent = getPercent(minVal);
-      const maxPercent = getPercent(maxValRef.current);
-
-  
-      if (range.current) {
-        range.current.style.left = `${minPercent}%`;
-        range.current.style.width = `${maxPercent - minPercent}%`;
-      }
-    }, [minVal, getPercent]);
-  
-    // Set width of the range to decrease from the right side 
-    useEffect(() => {
-      const minPercent = getPercent(minValRef.current);
-      const maxPercent = getPercent(maxVal);
-  
-      if (range.current) {
-        range.current.style.width = `${maxPercent - minPercent}%`;
-      }
-    }, [maxVal, getPercent]);
   
     return (
       <div className="container">
@@ -54,7 +29,6 @@ export default function MinMaxSlider({sliderMin,sliderMax,minAtom,maxAtom}:MinMa
           onChange={(event) => {
             const value = Math.min(Number(event.target.value), maxVal);
             setMinVal(value);
-            minValRef.current = value;
           }}
           className="thumb thumb--left"
         />
@@ -67,15 +41,14 @@ export default function MinMaxSlider({sliderMin,sliderMax,minAtom,maxAtom}:MinMa
           onChange={(event) => {
             const value = Math.max(Number(event.target.value), minVal);
             setMaxVal(value);
-            maxValRef.current = value;
           }}
           className="thumb thumb--right"
-
         />
   
         <div className="slider">
           <div className="slider__track" />
-          <div ref={range} className="slider__range" />
+          <div className="slider__range" />
+          <div className="slider__range" style={{ "--min-percent": getPercent(minVal), "--max-percent": getPercent(maxVal) }} />
           <div className="slider__left-value">{minVal}</div>
           <div className="slider__right-value">{maxVal}</div>
         </div>
