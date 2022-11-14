0c1d3e9d6eeb1bd977244494f75a9fca81f27837
Donghyeon Kim <0916dhkim@gmail.com>
2022-11-13 22:45:06 -0500

    Fix app type error

--- app/src/components/MinMaxSlider.tsx
+++ app/src/components/MinMaxSlider.tsx
@@ -1,6 +1,7 @@
-import { ReactElement,useCallback} from "react";
-import { useAtom, PrimitiveAtom } from "jotai";
 import "./MinMaxSlider.css";
+
+import { CSSProperties, ReactElement, useCallback } from "react";
+import { PrimitiveAtom, useAtom } from "jotai";
 
 type MinMaxSliderProps = {
   sliderMin: number;
@@ -8,50 +9,64 @@ type MinMaxSliderProps = {
   minInputAtom: PrimitiveAtom<number>;
   maxInputAtom: PrimitiveAtom<number>;
 };
-export default function MinMaxSlider({sliderMin,sliderMax,minInputAtom,maxInputAtom}:MinMaxSliderProps):ReactElement {
-    const [minVal, setMinVal] = useAtom(minInputAtom);
-    const [maxVal, setMaxVal] = useAtom(maxInputAtom);
-    
-    // Convert to percentage
-    const getPercent = useCallback(
-      (value:number) => Math.round(((value - sliderMin) / (sliderMax - sliderMin)) * 100),
-      [ sliderMin,  sliderMax]
-    );
-  
-    return (
-      <div className="container">
-        <input
-          type="range"
-          min={sliderMin}
-          max={sliderMax}
-          value={minVal}
-          step={sliderMax-sliderMin == 1 ? 0.1 : 1}
-          onChange={(event) => {
-            const value = Math.min(Number(event.target.value), maxVal);
-            setMinVal(value);
-          }}
-          className="thumb thumb--left"
+export default function MinMaxSlider({
+  sliderMin,
+  sliderMax,
+  minInputAtom,
+  maxInputAtom,
+}: MinMaxSliderProps): ReactElement {
+  const [minVal, setMinVal] = useAtom(minInputAtom);
+  const [maxVal, setMaxVal] = useAtom(maxInputAtom);
+
+  // Convert to percentage
+  const getPercent = useCallback(
+    (value: number) =>
+      Math.round(((value - sliderMin) / (sliderMax - sliderMin)) * 100),
+    [sliderMin, sliderMax]
+  );
+
+  return (
+    <div className="container">
+      <input
+        type="range"
+        min={sliderMin}
+        max={sliderMax}
+        value={minVal}
+        step={sliderMax - sliderMin == 1 ? 0.1 : 1}
+        onChange={(event) => {
+          const value = Math.min(Number(event.target.value), maxVal);
+          setMinVal(value);
+        }}
+        className="thumb thumb--left"
+      />
+      <input
+        type="range"
+        min={sliderMin}
+        max={sliderMax}
+        value={maxVal}
+        step={sliderMax - sliderMin == 1 ? 0.1 : 1}
+        onChange={(event) => {
+          const value = Math.max(Number(event.target.value), minVal);
+          setMaxVal(value);
+        }}
+        className="thumb thumb--right"
+      />
+
+      <div className="slider">
+        <div className="slider__track" />
+        <div className="slider__range" />
+        <div
+          className="slider__range"
+          style={
+            {
+              "--min-percent": getPercent(minVal),
+              "--max-percent": getPercent(maxVal),
+            } as CSSProperties
+          }
         />
-        <input
-          type="range"
-          min={sliderMin}
-          max={sliderMax}
-          value={maxVal}
-          step={sliderMax-sliderMin == 1 ? 0.1 : 1}
-          onChange={(event) => {
-            const value = Math.max(Number(event.target.value), minVal);
-            setMaxVal(value);
-          }}
-          className="thumb thumb--right"
-        />
-  
-        <div className="slider">
-          <div className="slider__track" />
-          <div className="slider__range" />
-          <div className="slider__range" style={{ "--min-percent": getPercent(minVal), "--max-percent": getPercent(maxVal) }} />
-          <div className="slider__left-value">{minVal}</div>
-          <div className="slider__right-value">{maxVal}</div>
-        </div>
+        <div className="slider__left-value">{minVal}</div>
+        <div className="slider__right-value">{maxVal}</div>
       </div>
-    );
+    </div>
+  );
 }
