ebd36f51f7cd75a10652b65f9bf836244e3b56f6
Donghyeon Kim <0916dhkim@gmail.com>
2022-11-13 22:25:11 -0500

    Fix database service

--- server/src/services/database/index.ts
+++ server/src/services/database/index.ts
@@ -11,29 +11,46 @@ export const DatabaseService = () => {
   });
 
   return {
-    getAuthDoc: (uid: string) => SpotifyAuth.findById(uid).exec(),
-    updateAuthDoc: (uid: string, data: Partial<ISpotifyAuth>) =>
-      SpotifyAuth.findByIdAndUpdate(uid, data),
-    createAuthDoc: (data: ISpotifyAuth) =>
-      SpotifyAuth.findOneAndReplace({ _id: data._id }, data, {
-        new: true,
-      }),
-    getAudioFeaturesCache: (trackId: string) =>
-      AudioFeaturesCache.findById(trackId).exec(),
-    setAudioFeaturesCache: (
+    getAuthDoc: async (uid: string) => {
+      const authDoc = await SpotifyAuth.findById(uid);
+      return authDoc?.toObject();
+    },
+    updateAuthDoc: async (uid: string, data: Partial<ISpotifyAuth>) => {
+      const authDoc = await SpotifyAuth.findByIdAndUpdate(uid, data);
+      return authDoc?.toObject();
+    },
+    createAuthDoc: async (data: ISpotifyAuth) => {
+      const authDoc = await SpotifyAuth.findOneAndUpdate(
+        { _id: data._id },
+        data,
+        {
+          upsert: true,
+          new: true,
+        }
+      );
+      return authDoc.toObject();
+    },
+    getAudioFeaturesCache: async (trackId: string) => {
+      const audioFeaturesDoc = await AudioFeaturesCache.findById(trackId);
+      return audioFeaturesDoc?.toObject().audioFeatures;
+    },
+    setAudioFeaturesCache: async (
       trackId: string,
       data: IAudioFeaturesCache["audioFeatures"]
-    ) =>
-      AudioFeaturesCache.findOneAndUpdate(
+    ) => {
+      const audioFeaturesDoc = await AudioFeaturesCache.findOneAndUpdate(
         { _id: trackId },
         {
           expiresAt: CACHE_LIFESPAN + Math.floor(new Date().getTime() / 1000),
           audioFeatures: data,
         },
         {
+          upsert: true,
           new: true,
         }
-      ).exec(),
+      );
+      return audioFeaturesDoc.toObject().audioFeatures;
+    },
     getProfile: async (uid: string): Promise<Profile> => {
       const authDoc = await SpotifyAuth.findById(uid);
       return {
