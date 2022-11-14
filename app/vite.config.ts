54e38b2bfaae3fd46d90f1673c972c9ec09d6d60
Donghyeon Kim <0916dhkim@gmail.com>
2022-11-14 13:45:42 -0500

    Change default backend port to 5001

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "http://localhost:5001",
     },
   },
 });
--- server/src/env.ts
+++ server/src/env.ts
@@ -13,7 +13,7 @@ function requireEnv(name: string): string {
 export const APP_BASE_URL = process.env.APP_BASE_URL ?? "http://localhost:5173";
 export const CACHE_LIFESPAN = process.env.CACHE_LIFESPAN ?? "3600";
 export const MONGO_URI = requireEnv("MONGO_URI");
-export const PORT = process.env.PORT ?? "5000";
+export const PORT = process.env.PORT ?? "5001";
 export const SESSION_SECRET = requireEnv("SESSION_SECRET");
 export const SPOTIFY_CLIENT_ID = requireEnv("SPOTIFY_CLIENT_ID");
 export const SPOTIFY_CLIENT_SECRET = requireEnv("SPOTIFY_CLIENT_SECRET");
