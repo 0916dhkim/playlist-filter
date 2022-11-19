44ca060a7d0af2168da3675407d5d93924da7b15
Donghyeon Kim <0916dhkim@gmail.com>
2022-11-18 21:44:39 -0500

    Add msw for testing

--- app/src/pages/SignIn.cy.tsx
+++ app/src/pages/SignIn.cy.tsx
@@ -1,10 +1,18 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { mount } from "cypress/react18";
+import { rest } from "msw";
 import { BrowserRouter } from "react-router-dom";
+import { worker } from "../mockHandlers";
 import { queryClient } from "../queryClient";
 import SignIn from "./SignIn";
 
 it("default", () => {
+  worker.use(
+    rest.get("/api/profile", (req, res, ctx) => {
+      return res(ctx.status(403));
+    })
+  );
+
   mount(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
